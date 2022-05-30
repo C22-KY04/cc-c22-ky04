4554577321c8f02fc72494ca3f388d416de281ca
Arya Salsabila <aryasalsabila789@gmail.com>
2022-05-30 18:29:17 +0700

    Update deploy.sh

--- cloud-run-deployment/deploy.sh
+++ cloud-run-deployment/deploy.sh
@@ -1,10 +1,11 @@
 GOOGLE_PROJECT_ID=# Google Cloud Platform Project ID
 
-gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/idcardsapi \
+gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/idcards-api \
   --project=$GOOGLE_PROJECT_ID
 
-gcloud beta run deploy barkbark-api \
-  --image gcr.io/$GOOGLE_PROJECT_ID/idcardsapi \
+gcloud beta run deploy idcards-api \
+  --image gcr.io/$GOOGLE_PROJECT_ID/idcards-api \
   --platform managed \
   --region asia-southeast2 \
-  --project=$GOOGLE_PROJECT_ID
+  --project=$GOOGLE_PROJECT_ID \
+  --allow-unauthenticated

79b0b740d8fdd1941af5f34bc124b7a3fb48f49c
arryaaas <aryasalsabila789@gmail.com>
2022-06-02 14:56:04 +0700

    switch to bearer token

--- cloud-run-deployment/src/middlewares/auth.js
+++ cloud-run-deployment/src/middlewares/auth.js
@@ -1,8 +1,8 @@
 const admin = require('../config/firebase');
 
 const getIdToken = (req, res, next) => {
-  if (req.headers.authorization) {
-    req.idToken = req.headers.authorization;
+  if (req.headers.authorization.startsWith('Bearer ')) {
+    req.idToken = req.headers.authorization.split('Bearer ').pop();
   } else {
     req.idToken = null;
   }
@@ -23,7 +23,7 @@ const isAuthenticated = (req, res, next) => {
     } catch (e) {
       return res.status(401).json({
         status: 'Unauthorized',
-        message: 'You are not authorized to make this request',
+        message: 'You do not have permissions to access the service.',
       });
     }
   });

fd9dbec139dc01709e84175e3ef552a2f2395b80
Bayu Eka Prawira <bayueka980@gmail.com>
2022-06-03 15:19:43 +0700

    Feat: Add download from Cloud Storage to Local in Cloud Function

--- cloud-function-deployment/main.py
+++ cloud-function-deployment/main.py
@@ -17,6 +17,35 @@ def upload_to_bucket(bucket_name, source_file, blob_name):
 
     return blob.public_url
 
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your GCS object
+    # source_blob_name = "storage-object-name"
+
+    # The path to which the file should be downloaded
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
+
+
 @functions_framework.http
 def handle_http_method(request):
     bucket_name = "my-bucket-24052022"

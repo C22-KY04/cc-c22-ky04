a791f42db2e85fec7a66e149b1d3b79a40ef6faa
modalhoki <72333392+modalhoki@users.noreply.github.com>
2022-06-07 01:35:56 +0700

    fix bugs

--- identify-ocr-api/deploy.sh
+++ identify-ocr-api/deploy.sh
@@ -5,7 +5,7 @@ gsutil cp gs://idcard-identification-models/model.h5 ./modules/identify/
 gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/identify-ocr-api \
   --project=$GOOGLE_PROJECT_ID
 
-gcloud beta run deploy ocr-api \
+gcloud beta run deploy identify-ocr-api \
   --image gcr.io/$GOOGLE_PROJECT_ID/identify-ocr-api \
   --platform managed \
   --region asia-southeast2 \

3397b5244d8c2816387da44e9d9ecbe44168c648
modalhoki <72333392+modalhoki@users.noreply.github.com>
2022-06-07 01:22:38 +0700

    Update deploy.sh

--- identify-ocr-api/deploy.sh
+++ identify-ocr-api/deploy.sh
@@ -1,6 +1,6 @@
 GOOGLE_PROJECT_ID=# Google Cloud Platform Project ID
 
-gsutil cp gs://my-bucket/model.h5 ./modules/identify/
+gsutil cp gs://idcard-identification-models/model.h5 ./modules/identify/
 
 gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/identify-ocr-api \
   --project=$GOOGLE_PROJECT_ID

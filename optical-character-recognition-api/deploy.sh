61990834c6ee35a4bee00705e31238eb1720149c
Bayu Eka Prawira <bayueka980@gmail.com>
2022-06-11 09:19:44 +0700

    Add gdown command to download model

--- optical-character-recognition-api/deploy.sh
+++ optical-character-recognition-api/deploy.sh
@@ -1,7 +1,5 @@
 GOOGLE_PROJECT_ID=# Google Cloud Platform Project ID
 
-gsutil cp gs://idcard-identification-models/model.h5 ./modules/classification/model.h5
-
 gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/ocr-api \
   --project=$GOOGLE_PROJECT_ID
 

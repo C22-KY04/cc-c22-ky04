28311ff087efce57365307e308c2578733f9388b
arryaaas <aryasalsabila789@gmail.com>
2022-06-14 08:11:39 +0700

    added function to delete object from bucket

--- optical-character-recognition-api/modules/storage/function.py
+++ optical-character-recognition-api/modules/storage/function.py
@@ -18,3 +18,10 @@ def download_from_bucket(bucket_name, source_blob_name, destination_file_name):
 
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(destination_file_name)
+
+def delete_from_bucket(bucket_name, source_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(source_blob_name)
+    blob.delete()
